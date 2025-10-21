900ea4eae9d5f2ced4b3dd55211240b841a81a0e
Javier Borjas <cristman11@gmail.com>
2025-10-21 00:35:56 -0300

    Creación y Registro de Roles | Controlador, Rutas y Formulario

---
+++ resources/views/admin/roles/create.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Creación de un nuevo rol</h1>
+    <hr>
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Llene los campos del formulario</h4>
+                </div>
+                <div class="card-body">
+                    <form action="{{ url('/admin/roles/create') }}" method="POST">
+                        @csrf
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="name">Nombre del rol <sup class="text-danger">(*)</sup></label>
+                                    <div class="input-group">
+                                        <span class="input-group-text"><i class="bi bi-person-badge-fill"></i></span>
+                                        <input type="text" name="name" id="name" class="form-control"
+                                            placeholder="Nombre del rol">
+                                    </div>
+                                    @error('name')
+                                        <small class="text-danger">{{ $message }}</small>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <a href="{{ url('/admin/roles') }}" class="btn btn-secondary">Cancelar</a>
+                                    <button type="submit" class="btn btn-primary">Registrar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/admin/roles/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Listado de roles</h1>
+    <hr>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Roles registrados</h4>
+                </div>
+                <div class="card-body">
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection

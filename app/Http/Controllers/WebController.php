ab49dbc2ad0bb44643f58ea0970d10ef120a4a6b
Javier Borjas <cristman11@gmail.com>
2025-11-10 00:53:55 -0300

    PRODUCTOS DINÁMICOS: Carga desde BD y Visualización en la Tienda

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ajuste;
+use App\Models\Producto;
 use Illuminate\Http\Request;
 
 class WebController extends Controller
 {
     public function index()
     {
-        return view('web.index');
+        $ajuste = Ajuste::first();
+        $productos = Producto::all();
+        return view('web.index', compact('ajuste', 'productos'));
     }
 }

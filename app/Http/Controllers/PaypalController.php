2a62b54b20c4c58efee1002252a227f345470ba7
Javier Borjas <cristman11@gmail.com>
2025-11-24 19:16:34 -0300

    PAGO COMPLETADO y REGISTRO de Pedido | Lógica Final de Venta con PayPal

--- app/Http/Controllers/PaypalController.php
+++ app/Http/Controllers/PaypalController.php
@@ -40,31 +40,47 @@ class PaypalController extends Controller
 
         $response = $this->provider->createOrder($data);
 
-        dd($response);
+        // dd($response);
 
-        // try {
-        //     $response = $this->provider->createOrder($data);
+        try {
+            $response = $this->provider->createOrder($data);
 
-        //     if (isset($response['id']) && $response['id'] != null) {
-        //         // Redirigir al usuario a Paypal para completar el pago
-        //         foreach ($response['links'] as $link) {
-        //             if ($link['rel'] === 'approve') {
-        //                 return redirect()->away($link['href']);
-        //             }
-        //         }
-        //         return redirect()->route('web.carrito.index')->with('mensaje', 'No se pudo redirigir a PayPal.')->with('icono', 'error');
-        //     }else{
-        //         return redirect()->route('web.carrito.index')->with('mensaje', 'Error al crear la orden de PayPal')->with('icono', 'error');
-        //     }
-        // } catch (\Exception $e) {
-        //     return redirect()->route('web.carrito.index')->with('mensaje', 'Excepción capturada:' . $e->getMessage())->with('icono', 'error');
-        // }
+            if (isset($response['id']) && $response['status'] == 'CREATED') {
+                // Redirigir al usuario a Paypal para completar el pago
+                foreach ($response['links'] as $link) {
+                    if ($link['rel'] === 'approve') {
+                        return redirect()->away($link['href']);
+                    }
+                }
+                return redirect()->route('web.carrito.index')->with('mensaje', 'No se pudo redirigir a PayPal.')->with('icono', 'error');
+            }else{
+                return redirect()->route('web.carrito.index')->with('mensaje', 'Error al crear la orden de PayPal')->with('icono', 'error');
+            }
+        } catch (\Exception $e) {
+            return redirect()->route('web.carrito.index')->with('mensaje', 'Excepción capturada:' . $e->getMessage())->with('icono', 'error');
+        }
+
     }
 
-    public function gracias()
+    public function gracias(Request $request)
     {
+        $token = $request->query('token');
+        try{
+            $response = $this->provider->capturePaymentOrder($token);
 
+            if (isset($response['status']) && $response['status'] == 'COMPLETED') {
+                // Aqui puedes actualizar el estado del pedido en tu base de datos si es necesario
+
+                return redirect()->route('web.carrito.index')->with('mensaje', 'Pago realizado con éxito. ¡Gracias por tu compra!')->with('icono', 'success');
+            }else{
+                return redirect()->route('web.carrito.index')->with('mensaje', 'El pago no se completó correctamente.')->with('icono', 'error');
+            }
+        } catch (\Exception $e) {
+            return redirect()->route('web.carrito.index')->with('mensaje', 'Excepción capturada:' . $e->getMessage())->with('icono', 'error');
+        }
+                
     }
+    
 
     public function cancelar()
     {
